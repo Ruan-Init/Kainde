b6c560c4bbe42dce4aff91e56fdf1a2d4b3de478
Ruan-Init <ruan.espindola17@gmail.com>
2024-08-16 11:23:08 -0300

    remoção de botão

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,7 +8,6 @@ const Hero = () => {
      <div className="hero-text">
      <h1>Sobre a <span>Kainde</span> Tecnologia.</h1>
      <p>A Kainde Tecnologia é uma empresa inovadora no setor de Tecnologia da Informação, comprometida em oferecer soluções robustas, eficientes e escaláveis para empresas de todos os tamanhos. Com uma equipe altamente qualificada e certificada, nós nos dedicamos a transformar a TI em um diferencial competitivo para nossos clientes.</p>
-     <button className='btn-1'><Link to="/About">Explorar mais</Link></button>
      </div>
     </div>
   )
